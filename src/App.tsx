b23ee4cccbd9dfca97bddeeeab406da350571d6b
Henry McIntosh <henrymcintosh@henrys-air.home>
2021-09-24 19:58:11 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Example } from "./components/Example";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Example />
+    </div>
+  );
+}
+
+export default App;
